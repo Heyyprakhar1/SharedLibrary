4186bbbd9cdfe1254d2ac3fbb8bab1315913cd52
Heyyprakhar1 <Srivprak0106@gmail.com>
2025-08-19 19:07:37 +0530

    Create CICD.groovy

---
+++ vars/CICD.groovy
@@ -0,0 +1,11 @@
+def gitdownload(repo) 
+{
+  git branch: 'main', url: "https://github.com/Heyyprakhar1/SharedLibrary.git"
+}
+def build ()
+{
+  sh 'python3' 
+}
+def deployement(jobname . ip) {
+  
+}
